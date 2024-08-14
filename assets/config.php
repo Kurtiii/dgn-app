1876a8bc8239580321e2fbcef7f9065c436253a2
Kurt Krüger <KurtK06@outlook.de>
2024-08-14 16:54:03 +0200

    Installationsseite aktualisiert

--- assets/config.php
+++ assets/config.php
@@ -9,7 +9,7 @@ if ($_CONFIG['production']) {
     $_CONFIG['marks_url_os'] = 'https://schueler.domgymnasium-nmb.de/schueler/intern.php?do=noten';
     $_CONFIG['timetable_url'] = 'https://www.domgymnasium-nmb.de/plan/mobdaten/';
 } else {
-    $_CONFIG['base_url'] = 'http://localhost/kurtiii/dgn-app';
+    $_CONFIG['base_url'] = 'https://localhost/kurtiii/dgn-app';
     $_CONFIG['login_url'] = 'https://schueler.domgymnasium-nmb.de/login.php';
     $_CONFIG['marks_url'] = 'https://schueler.domgymnasium-nmb.de/schueler/intern.php?do=noten10';
     $_CONFIG['marks_url_os'] = 'https://schueler.domgymnasium-nmb.de/schueler/intern.php?do=noten';
--- views/install.view.php
+++ views/install.view.php
@@ -11,61 +11,85 @@
     <script src="a<?= $_CONFIG['base_url']; ?>/ssets/lib/bootstrap/bootstrap.min.js" defer></script>
     <script src="<?= $_CONFIG['base_url']; ?>/assets/lib/jquery/jquery.min.js"></script>
     <script src="<?= $_CONFIG['base_url']; ?>/assets/lib/fontawesome/fontawesome.min.js"></script>
-    <script src="<?= $_CONFIG['base_url']; ?>/assets/code/js/darkmode.js" defer></script>
+    <!-- <script src="<?= $_CONFIG['base_url']; ?>/assets/code/js/darkmode.js" defer></script> -->
     <link rel="manifest" href="<?= $_CONFIG['base_url']; ?>/manifest.json">
     <link rel="icon" type="image/png" href="<?= $_CONFIG['base_url']; ?>/assets/img/favicon.png" />
 </head>
 
 <body>
 
-    <div class="d-flex align-items-center justify-content-center" style="height: 100vh;">
-        <div class="text-center" style="max-width: 19rem;">
-            <i class="fa-duotone fa-arrow-down-to-line text-primary fa-2x mb-4"></i>
-            <h1 class="h5">Installieren</h1>
+    <div class="container">
+        <div class="text-center mt-5" style="max-width: 400px;">
+            <i class="fa-brands fa-android text-primary fa-2x"></i>
+            <h1 class="h5 mb-4">Android</h1>
 
-            <div class="accordion mt-5" id="accordionInstall">
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingOne">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseAndroid" aria-expanded="true" aria-controls="collapseAndroid">
-                            <i class="fa-brands fa-android me-2"></i>
-                            Android
-                        </button>
-                    </h2>
-                    <div id="collapseAndroid" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionInstall">
-                        <div class="accordion-body">
-                            Klicke auf <i class="fa-regular fa-ellipsis-vertical mx-2"></i> oben links und wähle "Zum Startbildschirm hinzufügen" bzw. "App installieren" aus.
-                            Folge danach den Anweisungen auf dem Bildschirm.
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingTwo">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseIOS" aria-expanded="false" aria-controls="collapseIOS">
-                            <i class="fa-brands fa-apple me-2"></i>
-                            iOS (iPhone / iPad)
-                        </button>
-                    </h2>
-                    <div id="collapseIOS" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionIOS">
-                        <div class="accordion-body">
-                            <div class="alert alert-primary text-start mb-3" role="alert">
-                                Du musst diese Schritte in Safari durchführen.
-                                Andere Browser unterstützen diese Funktion nicht.
-                            </div>
-                            Klicke auf <i class="fa-regular fa-arrow-up-from-square mx-2"></i> unten in der Mitte und wähle "Zum Startbildschirm hinzufügen" aus.
-                            Folge danach den Anweisungen auf dem Bildschirm.
-                        </div>
-                    </div>
-                </div>
+            <a href="https://play.google.com/store/apps/details?id=de.kurtiii.dgn_app">
+                <img src="<?= $_CONFIG['base_url']; ?>/assets/img/stores/playstore.png" alt="Google Play Store" height="40px">
+            </a>
+
+            <hr class="my-5">
+            <i class="fa-brands fa-apple text-primary fa-2x"></i>
+            <h1 class="h5 mb-4">iPhone &bull; iPad</h1>
+
+            <div id="pwa" style="display: none;">
+                <a href="#" id="btnInstall">
+                    <img src="<?= $_CONFIG['base_url']; ?>/assets/img/stores/pwa.png" alt="Install as PWA" height="40px">
+                </a>
+
             </div>
+
+            <div id="pwa-error" class="text-danger">
+                <i class="fa-regular fa-triangle-exclamation me-2"></i>
+                <b>Du verwendest einen veralteten Browser.</b>
+                Um diese App auf deinem <b>iPhone</b> zu installieren, öffne diese Seite in <b>Safari</b> und versuche es erneut.
+                Du kannst diese App auch <a href="<?= $_CONFIG['base_url']; ?>/web" class="text-danger">hier</a> im Browser verwenden.
+            </div>
+
+            <hr class="my-5">
+            <i class="fa-brands fa-windows text-primary fa-2x"></i>
+            <h1 class="h5 mb-4">Windows</h1>
+
+            <a href="https://www.microsoft.com/store/productId/9NVGBT5BDPM">
+                <img src="<?= $_CONFIG['base_url']; ?>/assets/img/stores/microsoft.svg" alt="Microsoft Store" height="40px">
+            </a>
+
+            <hr class="my-5">
+            <i class="fa-regular fa-globe text-primary fa-2x"></i>
+            <h1 class="h5 mb-4">Im Browser nutzen</h1>
+
+            <a href="<?= $_CONFIG['base_url']; ?>">
+                <img src="<?= $_CONFIG['base_url']; ?>/assets/img/stores/webapp.png" alt="Open as Webapp" height="40px">
+            </a>
 
         </div>
     </div>
+
+    <div class="my-5"></div>
 
     <script src="<?= $_CONFIG['base_url']; ?>/assets/lib/bootstrap/bootstrap.min.js"></script>
     <script>
         if (typeof navigator.serviceWorker !== 'undefined') {
             navigator.serviceWorker.register('<?= $_CONFIG['base_url']; ?>/assets/code/js/service-worker.js');
         }
+        let deferredPrompt;
+        window.addEventListener('beforeinstallprompt', (e) => {
+            deferredPrompt = e;
+            document.getElementById('pwa').style.display = 'block';
+            document.getElementById('pwa-error').style.display = 'none';
+        });
+
+        const installApp = document.getElementById('btnInstall');
+        installApp.addEventListener('click', async () => {
+            if (deferredPrompt !== null) {
+                deferredPrompt.prompt();
+                const {
+                    outcome
+                } = await deferredPrompt.userChoice;
+                if (outcome === 'accepted') {
+                    deferredPrompt = null;
+                }
+            }
+        });
     </script>
 </body>
 
